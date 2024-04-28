e4a338306e1641bf2da35de29c3fefe96b22a5e5
Edwin <edwinchia0710@gmail.com>
2024-04-29 00:22:13 +0800

    Add Counter

--- SimpleMultiplication.java
+++ SimpleMultiplication.java
@@ -3,29 +3,41 @@ import java.util.Random;
 
 public class SimpleMultiplication {
 
+    static int primitiveOperationCounter = 0;  // Make this static to access it in static methods
+
     public static void main(String[] args) {
-        Random random = new Random();
+        // Random random = new Random();
 
-        int n = 1 + random.nextInt(9); // Random digit length between 1 and 9
-        // int n = 5; // Example digit length, you can change this based on your requirements
-    
-        // Generate random numbers of n digits
-        String num1 = generateRandomNumber(n);
-        String num2 = generateRandomNumber(n);
-        
-        // String num1 = "12346"; // Example number 1
-        // String num2 = "54321"; // Example number 2
+        // int n = 1 + random.nextInt(9); // Random digit length between 1 and 9
+        // // int n = 5; // Example digit length, you can change this based on your requirements
 
-        System.out.println("Multiplying " + num1 + " and " + num2);
-        multiplyNumbers(num1, num2);
+        for(int n = 1; n < 20; n++) {
+            // Generate random numbers of n digits
+                String num1 = generateRandomNumber(n);
+                String num2 = generateRandomNumber(n);
+                
+                // String num1 = "12346"; // Example number 1
+                // String num2 = "54321"; // Example number 2
 
-        // Using BigInteger to handle large integer calculations
-        BigInteger bigNum1 = new BigInteger(num1);
-        BigInteger bigNum2 = new BigInteger(num2);
-        BigInteger result = bigNum1.multiply(bigNum2);
+                System.out.println("Multiplying " + num1 + " and " + num2);
+                multiplyNumbers(num1, num2);
 
-        System.out.println("The result is: " + result);
+                // Using BigInteger to handle large integer calculations
+                BigInteger bigNum1 = new BigInteger(num1);
+                BigInteger bigNum2 = new BigInteger(num2);
+                BigInteger result = bigNum1.multiply(bigNum2);
+
+                System.out.println("The result for the multiplication of " + n + " digit of number is: " + result);
+                System.out.println("The number of primitive operations is: " + primitiveOperationCounter);
+                System.out.println();
+                System.out.println();
+                primitiveOperationCounter = 0; // Reset the counter for the next run
+        }
     }
+
+
+    
+        
 
     private static String generateRandomNumber(int n) {
         StringBuilder number = new StringBuilder();
@@ -40,20 +52,41 @@ public class SimpleMultiplication {
         // Reverse the numbers to start multiplication from the rightmost digit
         num1 = new StringBuilder(num1).reverse().toString();
         num2 = new StringBuilder(num2).reverse().toString();
+
+        primitiveOperationCounter += 2; // Increment for two reversals
     
         // For storing the results of each multiplication step
         int[][] partialProducts = new int[num2.length()][num1.length()];
         int[][] carriers = new int[num2.length()][num1.length()];
+
+        primitiveOperationCounter += 2; // Increment for two array initializations
     
+        primitiveOperationCounter += 1;
         // Loop through each digit of num2 (multiplier)
         for (int i = 0; i < num2.length(); i++) {
+
+            primitiveOperationCounter += 4;
+            primitiveOperationCounter += 1;
+
             // Loop through each digit of num1 (multiplicand)
             for (int j = 0; j < num1.length(); j++) {
+
+                primitiveOperationCounter += 4;
+
                 int carry = 0; // Reset carry for the next iteration
+                primitiveOperationCounter += 1;
+
                 int product = (num2.charAt(i) - '0') * (num1.charAt(j) - '0') + carry;
+                primitiveOperationCounter += 7; // For addition, subtraction and multiplication operations
+
                 partialProducts[i][j] = product % 10; // Store the ones digit as partial product
+                primitiveOperationCounter += 2;
+
                 carry = product / 10; // Calculate the new carry
+                primitiveOperationCounter += 2;
+
                 carriers[i][j] = carry; // Store the tens digit as carry 
+                primitiveOperationCounter += 1;
             }
         }
 
@@ -83,12 +116,24 @@ public class SimpleMultiplication {
             System.out.println();
         }
 
+        primitiveOperationCounter += 1; // Increment for the loop
+
         for (int i = 0; i < num2.length(); i++) {
+            primitiveOperationCounter += 3;
+            primitiveOperationCounter += 1;
+
             for (int j = 0; j < num1.length(); j++) {
+
+                primitiveOperationCounter += 3;
                 // Calculate the value for each digit and carrier, shifted by its position
                 BigInteger baseValue = BigInteger.valueOf(partialProducts[i][j] + carriers[i][j] * 10);
+                primitiveOperationCounter += 4;
+
                 BigInteger positionalValue = baseValue.multiply(BigInteger.TEN.pow(i + j));
+                primitiveOperationCounter += 4;
+
                 result = result.add(positionalValue);
+                primitiveOperationCounter += 2;
             }
         }
     
